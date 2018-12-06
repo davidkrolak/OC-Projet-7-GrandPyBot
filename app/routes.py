763dd54f9d17a2dc521f354d99e7565097285d65
Axel-V <axel.vallee@icloud.com>
2018-12-06 18:40:12 +0100

    added a marker layer on the map

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, jsonify
 from app.api_calls import openstreetmap_calls, wikimedia_calls
+from app.search import search_script
 from app import app
 
 
@@ -17,12 +18,4 @@ def about():
 @app.route('/search', methods=['POST'])
 def search():
     user_request = request.form.get('search')
-    coordinates = openstreetmap_calls.search_lat_lon(user_request)
-    wikipedia_page_id = wikimedia_calls.search_page_id(user_request)
-    wiki_definition = wikimedia_calls.page_summary_text(wikipedia_page_id)
-    response = {"lat": coordinates[0],
-                "lon": coordinates[1],
-                "mapbox_token": app.config['MAPBOX_TOKEN'],
-                "wiki_definition": wiki_definition
-                }
-    return jsonify(response)
+    return jsonify(search_script(user_request))
