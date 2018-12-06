b9053c6eb8b736e571bb1c605bda223e69f488e4
Axel-V <axel.vallee@icloud.com>
2018-12-06 13:11:26 +0100

    position request from the client now display the map as requested

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+import json
+from flask import render_template, request, jsonify
+from app.api_calls import openstreetmap_calls, wikimedia_calls
 from app import app
 
 
@@ -12,9 +14,13 @@ def main():
 def about():
     return render_template('about.html')
 
+
 @app.route('/search', methods=['POST'])
 def search():
-    pass
-
-if __name__ == "__main__":
-    app.run()
+    user_request = request.form.get('search')
+    coordinates = openstreetmap_calls.search_lat_lon(user_request)
+    response = {"lat": coordinates[0],
+                "lon": coordinates[1],
+                "mapbox_token": app.config['MAPBOX_TOKEN']
+                }
+    return jsonify(response)
--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,19 +1,23 @@
-function map() {
-    // Must be used under a div with id='mapid'
+$("#search_request").on('click', function () {
 
-    var lat = server_response.lat;
-    var lon = server_response.lon;
-    var mapbox_token = server_response.mapbox_token;
+    var search_input = $("#search_input").val();
+    var search_query = {"search":search_input};
 
-    var mymap = L.map('mapid').setView([lat, lon], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: mapbox_token
-    }).addTo(mymap);
+    $.post("/search", search_query, function (data) {
+        var server_response = data;
+        var lat = server_response.lat;
+        var lon = server_response.lon;
+        var mapbox_token = server_response.mapbox_token;
 
-}
+        mymap.setView([lat, lon], 13);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: mapbox_token
+        }).addTo(mymap);
+    })
+});
 
 
 
