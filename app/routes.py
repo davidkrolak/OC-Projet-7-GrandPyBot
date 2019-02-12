ba76a876f0f9f5903a562fa9e04e2f89f7997f26
Axel-V <axel.vallee@icloud.com>
2019-02-12 11:18:22 +0100

    front end style edited

--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,27 @@ def user_msg():
 @app.route('/grandpy_message', methods=['POST'])
 def grandpy_message():
     user_request = request.form.get('user_input')
+
     r = research.Research(user_request)
-    return render_template(
-            'grandpy_message_1.html',
-            grandpy_response=r.grandpy_response
-    )
+    r.main()
+    response = {
+        'lat': r.lat,
+        'lng': r.lng,
+        'status': r.status,
+        'msg_1': render_template(
+                'grandpy_message_1.html',
+                grandpy_response=r.grandpy_response)
+    }
+
+    if r.status in research.wikimedia_ok_status:
+        response['msg_2'] = render_template('grandpy_message_2.html',
+                                            address=r.formatted_address)
+    elif r.status in research.no_info_status:
+        response['msg_2'] = render_template('grandpy_message_2.html',
+                                            address=r.formatted_address)
+
+    if r.status in research.wikimedia_ok_status:
+        response['msg_3'] = render_template('grandpy_message_3.html',
+                                            wiki_summary=r.wiki_summary,
+                                            wiki_link=r.wiki_url)
+    return jsonify(response)
--- app/static/js/response.js
+++ app/static/js/response.js
@@ -1,14 +1,19 @@
-$(".request_input_text").submit(function (event) {
+$("#request_input_form").submit(function (event) {
     event.preventDefault();
     var user_input = {"user_input": $("#research_input").val()};
 
     $.post("/user_message", user_input, function (data) {
         $("#research_input").val("");
-        $('.msg_history').append(data).scrollTop(1000000);
+        h = $('#msg_history').height();
+        console.log(h);
+        $('#msg_history').append(data);
+        $('#chat_box').scrollTop(h);
     });
 
-    $.post("/grandpy_message", user_input, function (data) {
-        $('.msg_history').append(data).scrollTop(1000000);
+    $.post("/grandpy_message", user_input, function (response) {
+        h = $('#msg_history').height();
+        console.log(h);
+        $('#msg_history').append(response.msg_1);
+        $('#chat_box').scrollTop(h);
     })
 });
-
